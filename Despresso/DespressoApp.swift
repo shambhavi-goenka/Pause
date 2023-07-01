e46370566ffc89d9ee810221bba0f88fa06f47bc
po-the-panda-12 <shambhavig.2019@scis.smu.edu.sg>
2023-07-01 14:30:21 +0800

    Initial Commit

---
+++ Despresso/DespressoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DespressoApp.swift
+//  Despresso
+//
+//  Created by Shambhavi Goenka on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DespressoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
